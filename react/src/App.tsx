bf527af01dfb1e0c1cd43bca39a66704818ce878
Kira-Vishn9 <Kvishn9@gmail.com>
2023-06-19 16:12:44 -0400

    add all change

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,30 +1,32 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import AuthContext from '../src/context/Context';
-import { LayoutDefault } from "./component/Layout/LayoutDefault";
-import Main from "./page/Main";
-import "./App.css";
-import LogIn from "./component/LogIn/LogIn";
-import { LayoutAuth } from "./component/Layout/LautAuth";
-import { useState } from "react";
-
-function App() {
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import AuthContext from '../src/context/Context'
+import { LayoutDefault } from './component/Layout/LayoutDefault'
+import Main from './page/Main'
+import './App.css'
+import LogIn from './component/LogIn/LogIn'
+import { LayoutAuth } from './component/Layout/LautAuth'
+import React from 'react'
+// Todo: make something
+function App (): JSX.Element {
   // auth - епременная, setAuth - () => void
-  const [auth, setAuth] = useState(false);
+  const [auth, setAuth] = React.useState(false)
 
   return (
     <AuthContext.Provider value={{ auth, setAuth }}>
       <BrowserRouter>
         <Routes>
-          <Route path="/main" element={<LayoutDefault children={Main} />} />
-          {auth ? (
-            <Route path="/LOGIN" element={<LayoutAuth children={LogIn} />} />
-          ) : (
+          <Route path="/main" element={<LayoutDefault><Main /> </LayoutDefault>} />
+          {auth
+            ? (
+              <Route path="/LOGIN" element={<LayoutAuth><LogIn /></LayoutAuth> } />
+              )
+            : (
             <Route path="/LOGIN" element={<LayoutDefault children={LogIn} />} />
-          )}
+              )}
         </Routes>
       </BrowserRouter>
     </AuthContext.Provider>
-  );
+  )
 }
 
-export default App;
+export default App
