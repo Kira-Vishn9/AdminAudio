eab376eadf65e3671da7ee0b747029bdf4a0a2f1
linkmss <59558119+linkmss@users.noreply.github.com>
2023-06-19 22:26:13 +0400

    dev: Eslint. WIP

--- react/src/component/Header/Header.tsx
+++ react/src/component/Header/Header.tsx
@@ -1,23 +1,27 @@
-import { Button } from "../index";
-import { Outlet, Link } from "react-router-dom";
-import { useLocation } from "react-router-dom";
-import React, { useContext } from "react";
-import AuthContext from "../../context/Context";
-import "./style.css";
+import { Button } from '../index'
+import { Link, useLocation } from 'react-router-dom'
+import React from 'react'
+import AuthContext from '../../context/Context'
+import './style.css'
 
-export function Header() {
-  const location = useLocation();
-  const [auth, setAuth] = useContext(AuthContext);
+export function Header (): JSX.Element {
+  const location = useLocation()
+  const { auth, setAuth } = React.useContext(AuthContext)
+
+  console.log(auth)
+  console.log(setAuth)
+
   return (
     <div className="header">
       <Link to="/main">
-        <span>Logo</span>
+        <span>logo</span>
       </Link>
+
       <Link to="/LOGIN">
         <Button color="primary" size="medium" variant="outlined">
-          {location.pathname === "/LOGIN" ? "Sign in" : "Sign up"}
+          {location.pathname === '/LOGIN' ? 'Sign in' : 'Sign up'}
         </Button>
       </Link>
     </div>
-  );
+  )
 }
--- react/src/component/LogIn/LogIn.tsx
+++ react/src/component/LogIn/LogIn.tsx
@@ -1,57 +1,55 @@
-import { Button, Input } from "../index";
-import { useContext, useState } from "react";
-import AuthContext from "../../context/Context";
-import { Outlet, Link } from "react-router-dom";
-import "./style.css";
+import { Button, Input } from '../index'
+import React from 'react'
+import './style.css'
 
-function LogIn() {
-  const [auth, setAuth] = useContext(AuthContext);
-  const [mail, setMail] = useState(false);
-  const [password, setPassword] = useState(false);
+function LogIn (): JSX.Element {
+  // const [auth, setAuth] = useContext(AuthContext);
+  const [mail, setMail] = React.useState(false)
+  const [password, setPassword] = React.useState(false)
 
-  const checkValidnoth = () => {
-    console.log(auth);
-  };
+  // const checkValidnoth = () => {
+  //   console.log(auth)
+  // }
 
   const validationMail = (e: InputEvent) => {
-    const check = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    const target = e?.target;
-    if (target === null) return;
+    const check = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    const target = e?.target
+    if (target === null) return
     if (target instanceof HTMLInputElement) {
-      setMail(check.test(target.value));
-      return check.test(target.value);
+      setMail(check.test(target.value))
+      return check.test(target.value)
     }
-  };
+  }
 
   const validationPassword = (e: InputEvent) => {
-    const check = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
-    const target = e.target;
-    if (target === null) return;
+    const check = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/
+    const target = e.target
+    if (target === null) return
     if (target instanceof HTMLInputElement) {
-      setPassword(check.test(target.value));
-      return check.test(target.value);
+      setPassword(check.test(target.value))
+      return check.test(target.value)
     }
-  };
+  }
 
   return (
     <div className="wrap-login">
       <div>
         <h3>Create your account</h3>
       </div>
+
       <Input
-        style={{ marginTop: "10px", marginBottom: "10px" }}
+        style={{ marginTop: '10px', marginBottom: '10px' }}
         placeholder="Enter your mail or login"
         error={!mail}
         onChange={validationMail}
       />
       <Input
-        style={{ marginTop: "10px", marginBottom: "10px" }}
+        style={{ marginTop: '10px', marginBottom: '10px' }}
         placeholder="Enter your password"
         error={!password}
         onChange={validationPassword}
       />
       <Button
-        onClick={checkValidnoth}
         color="primary"
         size="medium"
         variant="outlined"
@@ -59,7 +57,7 @@ function LogIn() {
         Enter
       </Button>
     </div>
-  );
+  )
 }
 
-export default LogIn;
+export default LogIn
