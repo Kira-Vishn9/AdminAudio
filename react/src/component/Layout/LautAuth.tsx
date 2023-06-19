13c330aec9ce3568e5ff3927fc3a539034322e91
222 <kiravishn9@gmail.com>
2023-06-19 14:32:57 -0400

    pass

--- react/src/component/Layout/LautAuth.tsx
+++ react/src/component/Layout/LautAuth.tsx
@@ -1,19 +1,19 @@
-import { Header } from "../Header/Header"
-import { Aside } from "../Aside/Aside"
-import "./style.css"
+import { Header } from "../Header/Header";
+import { Aside } from "../Aside/Aside";
+import "./style.css";
 
-export function LayoutAuth ({children: Children, isAuth= false}) {
-    return(
-        <div className="layout">
-            <div className="wrap-header">
-                <Header></Header>
-            </div>
-            <div className={isAuth ? "right-template" : "flex-template"}>
-              <div className="wrap-aside">
-                <Aside></Aside>
-              </div>
-            <main className="main">{<Children />}</main>
-            </div>
+export function LayoutAuth({ children: Children, isAuth = false }) {
+  return (
+    <div className="layout">
+      <div className="wrap-header">
+        <Header></Header>
+      </div>
+      <div className={isAuth ? "right-template" : "flex-template"}>
+        <div className="wrap-aside">
+          <Aside></Aside>
         </div>
-    )
+        <main className="main">{<Children />}</main>
+      </div>
+    </div>
+  );
 }
